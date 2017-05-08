075680c6cd1160d64d7b4241ee64abe2cb9b4ada
Tuan Phan <tuan230878@yahoo.com>
2017-05-08 18:26:10 +1000

    add controllers

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ models/genre.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GenreSchema = Schema({
+    name: { type: String, required: true, min:3, max:100}
+});
+
+// Virtual for bookinstance's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
